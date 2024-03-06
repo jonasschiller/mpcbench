1c8c87b30e0d332bb76ae2d426a60bc1b2cc7071
Schiller Jonas <q533604@europe.bmw.corp>
2024-03-06 14:14:06 +0100

    Upload to files

--- helpers/pos_helper.sh
+++ helpers/pos_helper.sh
@@ -32,7 +32,7 @@ done
 
 echo "  loading variables files on host(s) ${NODES[*]}"
 
-rm "experiments/run-variables.yml"
+rm -f "experiments/run-variables.yml"
 runtimepath="experiments/run-variables.yml"
 parameters="$FRAMEWORK"
 echo "framework: ${parameters// /, }" >> "$runtimepath"
@@ -41,7 +41,7 @@ echo "experiment: ${parameters// /, }" >> "$runtimepath"
 for node in "${NODES[@]}"; do
 	# default variables file for all experiments
 	{ "$POS" alloc set_var "$node" global-variables.yml --as-global;
-	"$POS" alloc set_var "$node" runtimepath.yml --as-global;
+	"$POS" alloc set_var "$node" "experiments/run-variables.yml" --as-global;
 	# default variables file for concrete experiment"
 	# loop variables for experiment script (append random num to mitigate conflicts)
 	loopvarpath="experiments/loop-variables-$NETWORK.yml"
