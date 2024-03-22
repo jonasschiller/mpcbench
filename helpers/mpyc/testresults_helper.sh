57c574e47382cab89c66c4c139bc61dddd4a720d
Schiller Jonas <q533604@europe.bmw.corp>
2024-03-22 17:08:53 +0100

    Updated Git Export

--- helpers/mpyc/testresults_helper.sh
+++ helpers/mpyc/testresults_helper.sh
@@ -135,8 +135,8 @@ exportExperimentResults() {
         # a pull is not really required, but for small sizes it doesn't hurt
         git pull
         # copy from local folder to git repo folder
-        [ ! -d "${EXPORTPATH::-12}" ] && mkdir results/"${EXPORTPATH::-12}"
-        cp -r ../"$EXPORTPATH" "${EXPORTPATH::-12}"
+        [ ! -d "$EXPORTPATH" ] && mkdir -p "$EXPORTPATH"
+        cp -r ../"$EXPORTPATH" "${EXPORTPATH::-11}"
         git add . 
         git commit -a -m "script upload"
         git push 
